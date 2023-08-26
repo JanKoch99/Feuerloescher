d633995a94e0d6f8ba0032ea0a00cb742d3f0b71
Z4l3s5i0 <79455975+Z4l3s5i0@users.noreply.github.com>
2023-08-27 00:59:48 +0100

    Update raspController.js

--- backend/controllers/raspController.js
+++ backend/controllers/raspController.js
@@ -17,7 +17,7 @@ const userConnection = async (req, res) => {
 const triggerAlarm = async (req, res) => {
     const {id} = req.params
 
-    const raspConnections = await RaspConnection.find({rasp_id: id}).sort({debug: 1})
+    const raspConnections = await RaspConnection.find({rasp_id: id}).sort({debug: -1})
     console.log(raspConnections)
 
     if (!raspConnections) {
