c3ba6d5f1d3746b5dafda3772777b7d094c11ce8
Z4l3s5i0 <79455975+Z4l3s5i0@users.noreply.github.com>
2023-08-26 14:50:41 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const {connection} = require("mongoose");
-const cors = require('cors'); // Import the cors package
-
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 const userConnectionRoutes = require('./routes/rasp')
@@ -18,7 +16,6 @@ app.use(cors({
 
 // middleware
 app.use(express.json())
-app.use(cors()); // Use the cors middleware
 
 
 const WS_PORT = process.env.PORT2;
