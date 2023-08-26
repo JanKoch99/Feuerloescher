ed32ee268fb1b3b3d61f29d6b9a2caf913b7a49c
Z4l3s5i0 <79455975+Z4l3s5i0@users.noreply.github.com>
2023-08-26 12:11:26 +0100

    adjustements websocket

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
 require('dotenv').config()
-
 const express = require('express')
 const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-const userRoutes = require('./routes/user')
-const userConnectionRoutes = require('./routes/rasp')
 const http = require("http");
 const {connection} = require("mongoose");
 const path = require("path");
+const cors = require('cors'); // Import the cors package
+const socketIO = require('socket.io')
+
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+const userConnectionRoutes = require('./routes/rasp')
+
 
 const app = express()
 const server = http.createServer(app)
-const io = socketIO(server)
+
+// middleware
+app.use(express.json())
+app.use(cors()); // Use the cors middleware
+
+
+let io = socketIO(server);
+
 //videoStream
 io.on('connection', (socket) => {
     console.log('A new Websocket connection has been established.')
@@ -22,19 +32,16 @@ io.on('connection', (socket) => {
     })
 })
 
-
-// middleware
-app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-app.use(express.static(path.join(__dirname, 'frontend/build')))
+// Serve the React frontend static files
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.use('/api/workouts',workoutRoutes)
 app.use('/api/user',userRoutes)
 app.use('/api/rasp', userConnectionRoutes)
-
 
 
 mongoose.connect(process.env.MONG_URI)
