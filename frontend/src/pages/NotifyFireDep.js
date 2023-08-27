f5393cb91c9b947c3c806df51ceb313500816d61
JanKoch99 <jankoch99@bluewin.ch>
2023-08-27 11:04:47 +0200

    accepted

--- frontend/src/pages/NotifyFireDep.js
+++ frontend/src/pages/NotifyFireDep.js
@@ -3,12 +3,16 @@ import VideoFeed from "../components/VideoFeed";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFireAlt} from "@fortawesome/free-solid-svg-icons/faFireAlt";
 import {faTimes} from "@fortawesome/free-solid-svg-icons/faTimes";
-import {Card} from "react-bootstrap";
+import {Button, Card, Modal} from "react-bootstrap";
 import logo from "../images/logo.svg";
 
 
 const NotifyFireDep = () => {
     const [rasp_id, setRasp_id] = useState('')
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     useEffect(() =>{
         const queryParameters = new URLSearchParams(window.location.search)
         const raspId = queryParameters.get("rasp_id")
@@ -41,13 +45,22 @@ const NotifyFireDep = () => {
                         <div className="row mt-5 mb-3 mx-3">
                             <div className="col-12 d-flex justify-content-between">
                                 <a href="tel:144"className="btn btn-secondary w-48 d-flex justify-content-between align-items-center flex-column"><FontAwesomeIcon icon={faFireAlt} size="4x" style={{fontSize: 53, marginTop:5}} /><p className="mt-1 p-0 m-0">Notfall melden</p></a>
-                                <a className="btn btn-success w-48 d-flex justify-content-between align-items-center flex-column"><FontAwesomeIcon icon={faTimes} size="4x" /><p className="mt-1 p-0 m-0">Fehlalarm</p></a>
+                                <a className="btn btn-success w-48 d-flex justify-content-between align-items-center flex-column" onClick={handleShow}><FontAwesomeIcon icon={faTimes} size="4x" /><p className="mt-1 p-0 m-0">Fehlalarm</p></a>
                             </div>
                         </div>
                     </div>
                 </Card.Body>
             </Card>
-
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Body>
+                    <p>Da hattest du wohl Glück gehabt!</p>
+                    <div className="mt-3 justify-content-end d-flex">
+                        <Button variant="secondary" className="d-flex align-items-center justify-content-center me-2 mb-2" onClick={handleClose}>
+                            <FontAwesomeIcon icon={faTimes} /><span className="ms-2">Close</span>
+                        </Button>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
